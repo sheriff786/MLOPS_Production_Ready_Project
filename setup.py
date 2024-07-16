95ccc8d428da0645ba2bf9ef121214b973f8bdb1
sheriff786 <mohdsheriff27021996@gmail.com>
2024-07-16 15:09:13 +0530

    requirements.txt file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sheriff",
+    author_email="mdsheriff2702@gmail.com",
+    packages=find_packages()
+)
