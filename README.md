e6856a9d8b21ea5e45639e92fff9a59f3ba8c70a
sheriff786 <mohdsheriff27021996@gmail.com>
2024-07-17 08:04:42 +0530

    data ingestion is done

--- README.md
+++ README.md
@@ -32,3 +32,10 @@ conda activate visa
 
 ```bash
 pip install -r requirements.txt
+
+## Workflo:
+
+1.constants
+2.entity
+3.components
+4.pipeline
--- demo.py
+++ demo.py
@@ -1,8 +1,18 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-logging.info("Welcome to our custom log")
-import sys
-try:
-    a=2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# logging.info("Welcome to our custom log")
+# import sys
+# try:
+#     a=2/0
+# except Exception as e:
+#     raise USvisaException(e,sys)
+
+# import os
+# mongo_db_url = os.getenv('MONGODB_URL')
+# print(mongo_db_url)
+
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj=TrainPipeline()
+obj.run_pipeline()
